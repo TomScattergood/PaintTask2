03becd98e31226e5e974c9a456daf0e93eb59c6c
Thomas Scattergood <tscattergood@softwareinstitute.com>
2021-10-12 11:11:29 +0100

    I added lines of code to the programme

--- src/com/softwareinstitute/training/scattergood/thomas/Main.java
+++ src/com/softwareinstitute/training/scattergood/thomas/Main.java
@@ -7,24 +7,51 @@ public class Main
 
     public static void main(String[] args)
     {
-        Double width, height, area, price, cost, quantity, amount, coverage;
+        //Declaring variables
+        Double width;
+        Double height;
+        Double wallArea;
+        Double price;
+        Double cost;
+        Double quantity;
+        Double amount;
+        Double coverage;
+        Double widthWindow;
+        Double heightWindow;
+        Double windowArea;
 
+        // Input height of room
         Scanner wallHeight = new Scanner(System.in);
         System.out.println("Enter height of wall in m");
         height = wallHeight.nextDouble();
 
+        //Input width of room
         Scanner wallWidth = new Scanner(System.in);
         System.out.println("Enter width of wall in m");
         width = wallWidth.nextDouble();
 
-        area = width * height;
-                System.out.println("Area of wall is: " + area + "m^2");
+                //Input window width
+                Scanner windowWidth = new Scanner(System.in);
+                System.out.println("Enter the Width of window in m");
+                widthWindow = windowWidth.nextDouble();
 
+                //Input window height
+                Scanner windowHeight = new Scanner(System.in);
+                System.out.println("Enter the height of window in m");
+                heightWindow = windowHeight.nextDouble();
+                windowArea = heightWindow * widthWindow;
+
+                wallArea = (width * height) - (widthWindow * heightWindow); //Area of room is calculated
+                System.out.println("Area of wall is: " + wallArea + "m^2");
+
+                //Input amount of walls being painted
                 Scanner wallAmount = new Scanner(System.in);
                 System.out.println("Enter the number of walls being painted"); //assuming all walls have same dimensions
                 amount = wallAmount.nextDouble();
-                System.out.println("Total area of paint needed = " + area * amount + "m^2");
+                System.out.println("Total area of paint needed = " + wallArea * amount + "m^2");
 
+
+                //Input how much 1 tin of paint covers in m^2
                 Scanner paintCoverage = new Scanner(System.in);
                 System.out.println("Enter the coverage per tin in metres squared");
                 coverage = paintCoverage.nextDouble();
@@ -33,7 +60,7 @@ public class Main
         System.out.println("Enter the price per tin of paint");
         price = paintPrice.nextDouble();
 
-        quantity = Math.ceil(area/coverage)*amount;
+        quantity = Math.ceil(wallArea/coverage)*amount;
         cost = quantity * price;
         System.out.println("The quantity of paint tins required will be " + quantity + ".");
         System.out.printf("The price of the paint required will be £%.2f %n", cost);
